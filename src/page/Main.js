9838e69aca064ab7903539c9a4a445592bf5aa00
WONHO22 <dnjsgh2122@gmail.com>
2023-07-11 16:42:55 +0900

    [Feat] Main페이지 컴포넌트 기본 구조 구현 완료

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 50px;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Main = () => {
   return (
     <>
       <Header />
-      <div>메인페이지요</div>
+      <Container>
+        <div>
+          상품 리스트
+          <ItemWrapper>
+            상품 사진
+            <div>사진설명</div>
+          </ItemWrapper>
+        </div>
+        <div>
+          북마크 리스트
+          <ItemWrapper>
+            북마크 사진
+            <div>사진설명</div>
+          </ItemWrapper>
+        </div>
+      </Container>
       <Footer />
     </>
   );
