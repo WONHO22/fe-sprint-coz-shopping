872899ca631decb77051c4557ab8a7de59e0d953
WONHO22 <dnjsgh2122@gmail.com>
2023-07-13 10:50:11 +0900

    [Refactor] 리팩토링한 ProductModal컴포넌트 ProductListPage에 적용

--- src/page/ProductListPage.js
+++ src/page/ProductListPage.js
@@ -10,6 +10,7 @@ import brand from "../img/brand.png";
 import { FaStar } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 import ProductItem from "../component/ProductItem";
+import ProductModal from "../component/ProductModal";
 
 const FilteringSection = styled.section`
   padding-top: 30px;
@@ -42,64 +43,6 @@ const ProductContainer = styled.div`
     > img {
       width: 300px;
       height: 300px;
-    }
-  }
-`;
-
-const ProductModal = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.8);
-  z-index: 999;
-
-  > div {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-
-    > .closeButton {
-      > svg {
-        position: absolute;
-        top: 15px;
-        right: 0;
-        width: 70px;
-        height: 45px;
-        color: white;
-        cursor: pointer;
-      }
-    }
-
-    > .bookmarkStar {
-      > svg {
-        position: absolute;
-        bottom: 30px;
-        left: 20px;
-        width: 35px;
-        height: 35px;
-        color: white;
-        cursor: pointer;
-      }
-      > div {
-        position: absolute;
-        bottom: 30px;
-        left: 65px;
-        width: 400px;
-        height: 35px;
-        color: white;
-        cursor: pointer;
-        font-size: 1.6rem;
-      }
-    }
-
-    > img {
-      width: 1100px;
-      height: 680px;
-
-      border-radius: 15px;
     }
   }
 `;
@@ -162,51 +105,13 @@ const ProductListPage = ({ productData, setProductData, isBookmarked }) => {
         ))}
       </ProductContainer>
 
-      {/* 모달창 뜨는 부분 */}
-      {showModal && ( // 상품 이미지 클릭시 핸들러함수 clickModal 가 실행되어 showModal의 값이 바뀜 + 클릭한 사진이 SelectedImage 상태에 저장됨
-        <ProductModal onClick={() => setShowModal(false)}>
-          <div>
-            <div className="closeButton">
-              <IoClose />
-            </div>
-            <img
-              src={selectedImage.image_url}
-              alt={selectedImage.title}
-              onClick={(event) => event.stopPropagation()}
-            />
-            <div className="bookmarkStar">
-              {/* 모달창 내부 북마크 이미지 */}
-              <FaStar
-                onClick={(event) => {
-                  // 이벤트 캡처링으로 모달창 내부에서 클릭시 onClick={() => setShowModal(false)} 실행 방지
-                  event.stopPropagation();
-                  const newData = productData.map((data) => {
-                    // 모달창 내부 북마크 클릭시 => 클릭된 사진이 메인창에서 내가 클릭한 사진인지 확인
-                    // => 맞다면 isBookmarked 값을 바꾸어 줌(색상 및 isBookmarked값 변경)
-                    // => 변경된 isBookmarked 값을 productData에 업데이트
-                    if (data.id === selectedImage.id) {
-                      return {
-                        ...data,
-                        isBookmarked: !selectedImage.isBookmarked,
-                      };
-                    }
-                    return data;
-                  });
-                  setProductData(newData);
-                  // 모달창 내부 북마크 클릭시 현재 클릭된 사진(selectedImage)의 isBookmarked값을 변경시켜줘야하기 때문에 추가한 로직
-                  setSelectedImage((prevImage) => ({
-                    ...prevImage,
-                    isBookmarked: !prevImage.isBookmarked,
-                  }));
-                }}
-                fill={selectedImage.isBookmarked ? "#FFD361" : "white"}
-              />
-              {/* 해당 상품 항목의 북마크 상태에 따라 색상 설정 */}
-              <div>{selectedImage.title}</div>
-            </div>
-          </div>
-        </ProductModal>
-      )}
+      {/* 모달창 랜더링 부분 */}
+      <ProductModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        selectedImage={selectedImage}
+        handleBookmarkClick={handleBookmarkClick}
+      />
 
       <Footer />
     </>
